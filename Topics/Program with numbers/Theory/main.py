e1c4b23c9119db34eb1e90499d12d902603dca97
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2021-07-08 01:54:31 +0200

    Learning numbers

--- Topics/Program with numbers/Theory/main.py
+++ Topics/Program with numbers/Theory/main.py
@@ -1,2 +1,7 @@
-#  You can experiment here, it won’t be checked
+height = int(input())
+width = int(input())
+length = int(input())
 
+allowed = (height <= 10 < width <= 35 < length <= 40) or (height + width + length <= 80)
+
+print(allowed)
---
+++ Topics/While loop/Theory/main.py
@@ -0,0 +1,5 @@
+#  You can experiment here, it won’t be checked
+
+age = int(input())
+discount = (age < 21) or (age >= 65)
+print(discount)
