f87a56c1af5250b5cc4211e96d37760266e99cc7
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2021-07-07 20:47:14 +0200

    Print your name

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,2 +1,8 @@
-print("Hello! My name is Aid.")
-print("I was created in 2020.")
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+# reading a name
+name = input()
+print('What a great name you have, ', name)
+
---
+++ Topics/Taking input/Difference/main.py
@@ -0,0 +1,4 @@
+n1 = int(input())
+n2 = int(input())
+n = n1 - n2
+print(n)
